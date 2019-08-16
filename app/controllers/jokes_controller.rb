7c7a6a6db8437a543c85c30c52ad880bcd1d67fa
Stephen Macias <stephenmacias@yahoo.com>
2019-08-16 18:33:56 +0000

    finished quiz 2

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -4,4 +4,22 @@ class JokesController < ApplicationController
     @jokes = Joke.order("RANDOM()").first
   end
 
+  def new
+    @jokes = Joke.new
+  end
+
+  def create
+    Joke.create(joke_params)
+    if @joke.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered in invalid.'
+    end 
+    redirect_to root_path
+  end
+
+  private
+
+  def joke_params
+    params.require(:joke).permit(:joke)
+  end 
+
 end
--- app/models/joke.rb
+++ app/models/joke.rb
@@ -1,2 +1,3 @@
 class Joke < ApplicationRecord
+  validates :joke, presence: true, length: { maximum: 255, minimum: 1 }
 end
